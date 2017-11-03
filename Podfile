a78eb28e5a74322c758ad64179fa2ccdaa7353dd
Renato Mendes <renatodz86@gmail.com>
2017-11-03 16:02:54 -0200

    Add VIPER structure and Podfile with GoogleMaps pods.

---
+++ Podfile
@@ -0,0 +1,20 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '11.0’
+use_frameworks!
+
+def project_pods
+    # - Used in map
+    pod 'GoogleMaps'
+    pod 'GooglePlaces'
+
+    # - Used in cells
+    pod 'SDWebImage'
+end
+
+target 'Repair Search' do
+    project_pods
+end
+
+target 'Repair SearchTests' do
+    project_pods
+end
---
+++ Repair Search/Classes/Application/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Repair Search
+//
+//  Created by Renato Mendes on 03/11/17.
+//  Copyright © 2017 Renato Mendes. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder {
+
+    var window: UIWindow?
+
+}
+
+extension AppDelegate: UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow()
+        
+        return true
+    }
+    
+}
+
